89c28ffedeca4461f2968c966147ed6dc2b70a24
Antoine Albertelli <antoinea101@gmail.com>
2014-09-26 11:28:42 +0200

    Add some MessagePack tests
    Add some read write tests to check if the MessagePack implementation
    really works with the in-memory backend. It checks if we can write and
    read back a float, and also checks that the header is correct.

---
+++ tests/messagepack_tests.cpp
@@ -0,0 +1,31 @@
+#include "CppUTest/TestHarness.h"
+#include "../serialization.h"
+#include <cstring>
+
+TEST_GROUP(MessagePackTestGroup)
+{
+    cmp_ctx_t context;
+    serializer_t serializer;
+    char buffer[256];
+
+    void setup()
+    {
+        memset(buffer, 0, sizeof buffer);
+        serializer_init(&serializer, buffer, sizeof buffer);
+        serializer_cmp_ctx_factory(&context, &serializer);
+    }
+};
+
+TEST(MessagePackTestGroup, CanWriteFloat)
+{
+    float a = 3.14;
+    float b;
+    bool success;
+
+    cmp_write_float(&context, a);
+    success = cmp_read_float(&context, &b);
+
+    BYTES_EQUAL(0xca, buffer[0]); // float header
+    CHECK_TRUE(success);
+    CHECK_EQUAL(a, b);
+}
