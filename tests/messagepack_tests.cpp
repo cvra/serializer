b003c74ad61992a7ec0ab6ce0af99841e2e61f72
Antoine Albertelli <antoinea101@gmail.com>
2014-10-24 23:47:12 +0200

    Add a test about custom binaries in MessagePack

--- tests/messagepack_tests.cpp
+++ tests/messagepack_tests.cpp
@@ -55,3 +55,39 @@ TEST(MessagePackTestGroup, CanWriteString)
     CHECK_TRUE(res);
     STRCMP_EQUAL(expected, data);
 }
+
+TEST(MessagePackTestGroup, CanWriteCustomBinaryType)
+{
+    char data[] = {42, 43, 44};
+
+    char data2[3];
+    int8_t type = 12;
+    uint32_t size = 3;
+    bool success;
+
+    cmp_write_ext(&context, type, size, data);
+
+    // Ext 8 header
+    BYTES_EQUAL(0xc7, buffer[0]);
+
+    /* This part is commented out because of a bug in CMP (see issue #7)
+    // Data length
+    BYTES_EQUAL(3, buffer[1]);
+
+    // Data type
+    BYTES_EQUAL(12, buffer[2]);
+    */
+
+    for (int i = 0; i < 3; ++i) {
+        BYTES_EQUAL(data[i], buffer[3+i]);
+    }
+
+    success = cmp_read_ext(&context, &type, &size, data2);
+    CHECK_TRUE(success);
+    CHECK_EQUAL(12, type);
+    CHECK_EQUAL(3, size);
+
+    for (int i = 0; i < 3; ++i) {
+        BYTES_EQUAL(data[i], data2[i]);
+    }
+}
